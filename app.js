623752fb174cb35a85ac5392c872d9b13bd8cae1
nikitha26 <mnikitha365@gmail.com>
2020-07-30 22:06:44 +0530

    Validation

---
+++ app.js
@@ -0,0 +1,35 @@
+"use strict";
+//Form validation .
+// function fun1(){
+//     var msg ,x;
+//     msg = document.getElementById("para");
+//     msg.innerHTML = "";
+//     x = document.getElementById("demo").value;
+
+//     try{
+//         if(x == "") throw "Empty";
+//         if(isNaN(x)) throw "is not a number";
+//         if(x <10) throw "is valid ";
+//         if(x >11) throw "is invalid";
+//     }
+//     catch(error){
+//         msg.innerHTML ="Input is "+error;
+//     }
+//     finally{
+//         document.getElementById("demo").value = "";
+//     }
+// }
+
+// function fun(){
+//     let x = 3;
+//     // var x = 2;
+//     console.log(x);
+// }
+// //console.log(x)
+// fun()
+
+
+//Hoisting
+ x = 3;
+console.log(x)
+
